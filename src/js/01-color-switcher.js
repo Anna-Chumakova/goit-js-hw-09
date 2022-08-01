5a941c48e23fd5d6d76a3c172bbf72d094068891
Anna-Chumakova <ann020187@gmail.com>
2022-08-01 21:20:39 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const startEl = document.querySelector("[data-start]");
+const stopEl = document.querySelector("[data-stop]");
+const bodyEl = document.querySelector("body");
+
+
+let timerId = null;
+    
+    function onStartChangeColor(evt) {
+        
+        startEl.disabled = true;
+        stopEl.disabled = false;    
+        timerId = setInterval(function getRandomHexColor()  {
+        return bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }, 1000); 
+           
+}
+function onStopChangeColor(evt) {
+    stopEl.disabled = true;
+    startEl.disabled = false;
+    clearInterval(timerId);
+}
+
+startEl.addEventListener("click", onStartChangeColor);
+stopEl.addEventListener("click", onStopChangeColor);
