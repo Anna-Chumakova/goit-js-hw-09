22ff9463ba85a81cd694292ae80b806b9aef6381
Anna-Chumakova <ann020187@gmail.com>
2022-08-05 18:45:04 +0300

    task1, task2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,23 +3,28 @@ const startEl = document.querySelector("[data-start]");
 const stopEl = document.querySelector("[data-stop]");
 const bodyEl = document.querySelector("body");
 
-
+stopEl.disabled = true;
 let timerId = null;
+
+startEl.addEventListener("click", onStartChangeColor);
+stopEl.addEventListener("click", onStopChangeColor);    
     
-    function onStartChangeColor(evt) {
+function onStartChangeColor(evt) {
         
         startEl.disabled = true;
         stopEl.disabled = false;    
-        timerId = setInterval(function getRandomHexColor()  {
-        return bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        timerId = setInterval( () => {
+            return bodyEl.style.backgroundColor = getRandomHexColor();
     }, 1000); 
            
 }
+
 function onStopChangeColor(evt) {
-    stopEl.disabled = true;
     startEl.disabled = false;
+    stopEl.disabled = true;
     clearInterval(timerId);
 }
 
-startEl.addEventListener("click", onStartChangeColor);
-stopEl.addEventListener("click", onStopChangeColor);
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,69 +9,54 @@ const hoursEl = document.querySelector("[data-hours]");
 const minutesEl = document.querySelector("[data-minutes]");
 const secondsEl = document.querySelector("[data-seconds]");
 
-startBtnEl.disabled = true;
+startBtnEl.disabled = true; 
+
+const onClose = function (selectedDates) {
+
+    if (selectedDates[0] <= Date.now()) {
+            return Notify.info("Please choose a date in the future");
+       }
+startBtnEl.disabled = false;        
+
+startBtnEl.addEventListener("click", onTimer);
+    
+    function onTimer () {
+        setInterval(() => {
+            let deltaTime = selectedDates[0] - Date.now();
+            fillDate(deltaTime);
+        }, 1000);        
+    };      
+    
+};
+          
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-        let timerId = null;
-        if (selectedDates[0] <= Date.now()) {
-            clearInterval(timerId);
-            return Notify.info("Please choose a date in the future");
-        }
-        startBtnEl.disabled = false;
-        startBtnEl.addEventListener("click", onTimer);
-        function onTimer() {
-            timerId = setInterval(() => {
-                let deltaTime = selectedDates[0] - Date.now();
-                convertMs(deltaTime);
-            }, 1000);
-        }
-    }
+    onClose,
 };
 
 flatpickr(inputEl, options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+function fillDate(ms) {
+  
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-//   return { days, hours, minutes, seconds };
     daysEl.textContent = days;
     hoursEl.textContent = hours;
     minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
-
+    secondsEl.textContent = seconds; 
 }
 
-
-
-
- 
-   //    const seconds = Math.floor(deltaTime / 1000) % 60;
-    //    const minutes = Math.floor(deltaTime / 1000 / 60) % 60;
-    //    const hours = Math.floor(deltaTime / 1000 / 60 / 60) % 24;
-    //    const days = Math.floor(deltaTime / 1000 / 60 / 60 / 24);
-
-      //  secondsEl.textContent = seconds < 10 ? `0${seconds}` : seconds;
-        ///minutesEl.textContent = minutes < 10 ? `0${minutes}` : minutes;
-     //   hoursEl.textContent = hours < 10 ? `0${hours}` : hours;
-     //   dayEl.textContent = days < 10 ? `0${days}` : days;
-    
    
 
     
